93f786af9ab95be911fa7f7424ce18052302f60d
Chintala Rakshan <136550641+Rakshan0004@users.noreply.github.com>
2026-05-03 14:13:07 +0530

    playing with objects

--- main.py
+++ main.py
@@ -9,4 +9,37 @@ str2 = "world"
 def func():
     pass
 
-print(type(func))
+#print(type(func))
+
+str1 = "hello"
+
+new_str = str1.__len__()
+#print(new_str)
+
+
+#every thing in python is objects
+
+class Counter:
+    def __init__(self):
+        self.value = 0
+        
+    def count_down(self):
+        self.value -= 1
+    
+    def __str__(self):
+        return f"Count={self.value}"
+
+    def __add__(self, other):
+        return self.value + other.value
+
+    def count_up(self):
+        self.value += 1
+
+count1 = Counter()
+count2 = Counter()
+
+count1.count_up()
+count2.count_down()
+
+print(count1,  count2)
+print(count1 + count2)
